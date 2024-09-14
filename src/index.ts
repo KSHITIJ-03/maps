fbdeb2ce20c637e166407003a6093357b8f18e59
KSHITIJ-03 <yadavkshitij392001@gmail.com>
2024-09-14 13:05:51 +0530

    add : google maps api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log('hi there')
+/// <reference types="@types/google.maps"/>
+
+import { User } from './User'
+import { Company } from './Company.ts'
+
+// const user = new User()
+// const company = new Company()
+
+// console.log(user);
+// console.log(company);
+
+//console.log(google)
+
+// new google.maps.Map(document.getElementById('map') as HTMLElement, {
+//     zoom: 1,
+//     center: {
+//         lat: 0,
+//         lng: 0
+//     }
+// })
+
+// let map: google.maps.Map;
+// async function initMap(): Promise<void> {
+//   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+//   map = new Map(document.getElementById("map") as HTMLElement, {
+//     center: { lat: 0, lng: 0 },
+//     zoom: 1,
+//   });
+// }
+
+// initMap();
+
+let map: google.maps.Map;
+
+function initMap(): void {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: 0, lng: 0 },
+        zoom: 1,
+    });
+}
+
+// Ensure `initMap` is globally accessible for the API callback
+(window as any).initMap = initMap;
